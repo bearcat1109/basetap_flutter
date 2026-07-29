b19da41c494585623938d51c6fc32203cdfcb1b8
Gabriel Berres <gabrielberres9@gmail.com>
2026-07-29 10:56:27 -0500

    ASH plus flutter fixes

--- android/app/src/main/kotlin/com/gabrielberres/basetap_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.gabrielberres.basetap_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/dev/bearcat/basetap/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.bearcat.basetap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
